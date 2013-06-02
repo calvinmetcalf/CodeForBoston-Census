7011e17829c985521b42dbf4e41449778a9b688d
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-06-02 10:58:43 -0400

    less ugly

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var apiKey = '0a6b68796bfcfe694987a9ddf3eddd1b735dcd7f';
-var urlBase = 'http://api.census.gov/data/2011/acs5'
+var urlBase = 'http://api.census.gov/data/2011/acs5';
+var cKey='B19013_001E';
 var params = {
 	"key":apiKey,
-	"get":'B19013_001E',
+	"get":cKey,
 	"for":'county:*',
 	"in":'state:*'
 }
@@ -19,14 +20,14 @@ var attributionText = 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQ
 var mapquestSubdomains = '1234';
 
 var optionsObject = {
-    attribution : attributionText,
-    subdomains : mapquestSubdomains
+	attribution : attributionText,
+	subdomains : mapquestSubdomains
 }
 
 var mq=L.tileLayer(url, optionsObject).addTo(m);
 var watercolor = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg',{attribution:'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'})
 watercolor;
-
+var data;
 var rt = rTree();
 var obj={};
 var scale = d3.scale.quantile();
@@ -34,16 +35,16 @@ scale.range(d3.range(11));
 var layerControl = L.control.layers({"Stamen Watercolor":watercolor,"Map Quest Open":mq}).addTo(m);
 var counties = L.geoJson({features:[]},{
 	onEachFeature:function (f,l){
-    var out = [];
-    if (f.properties){
-        for(key in f.properties){
-            out.push(key+": "+f.properties[key]);
-        }
-        if(obj[f.id]){
-        out.push('income: $'+obj[f.id].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
-        }
-        l.bindPopup(out.join("<br />"));
-    }
+	var out = [];
+	if (f.properties){
+		for(key in f.properties){
+			out.push(key+": "+f.properties[key]);
+		}
+		if(obj[f.id]){
+			out.push('income: $'+obj[f.id].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+		}
+		l.bindPopup(out.join("<br />"));
+	}
 	},style:function(f){
 			return {fillColor:colorbrewer.Spectral[11][scale(obj[f.id])],weight:0,fillOpacity:0.8}
 		
@@ -52,23 +53,20 @@ var counties = L.geoJson({features:[]},{
 	layerControl.addOverlay(counties,"Counties");
 m.addHash({lc:layerControl});
 $.when($.ajax('json/us-counties.json'),$.get(urlBase,params,'json')).then(function(a,b){
-var data = a[0];
-var rows = b[0];
-var vals = [];
-_.each(rows,function(r){
-var val = parseInt(r[0],10);
-obj[r[1]+r[2]]=val;
-vals.push(val);
-});
-scale.domain(vals);
-rt.geoJSON(data,function(err,success){
+	data = a[0];
+	var rows = b[0];
+	var vals = [];
+	_.each(rows,function(r){
+		var val = parseInt(r[0],10);
+		obj[r[1]+r[2]]=val;
+		vals.push(val);
+	});
+	scale.domain(vals);
+	rt.geoJSON(data,function(err,success){
 		if(!err){
 			showAll();
 		}
 	});
-	
-
-
 });
 
 m.on("contextmenu moveend",showAll);
