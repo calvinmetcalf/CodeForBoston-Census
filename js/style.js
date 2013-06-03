97c37829ea9caec97f8b92e17d122c955c927164
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-06-03 07:57:54 -0400

    style fix

--- js/style.js
+++ js/style.js
@@ -4,16 +4,14 @@ var mapmargin = 50;
 	$(window).on("resize", resize);
 	resize();
 	function resize(){
-		$('#map').css("height", ($(window).height() - mapmargin));	
+		
 		if($(window).width()>=980){
+            $('#map').css("height", ($(window).height() - mapmargin));    
 			$('#map').css("margin-top",50);
 		}else{
-			$('#map').css("margin-top",-20);
+            $('#map').css("height", ($(window).height() - (mapmargin+12)));    
+			$('#map').css("margin-top",-21);
 		}
 
 	}
-	if($(window).width()>=980){
-		$('#map').css("margin-top",50);
-	}else{
-		$('#map').css("margin-top",-20);
-	}
+
