bf6df78ff08a2c8e11f6b3c3fa541a3005f85607
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-06-04 13:14:57 -0400

    updates including readme

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ CodeForBoston-Census
 ====================
 
 Repo for the census group.
+
+The data views are in the file `js/models.js` to add a new one you need to add another object to the array of the format
+
+```javascript
+{
+	name:'string',// required 
+	tables:'string',//required, one or more seperated by comma (only one string)
+	stringRep:function(a){
+				return  parseFloat(a,10).toPrecision(5);
+			},//optional defaults to this one, how the value is represented.
+	transform:'function'//optional if you need to calculate on multiple values
+}
+```
--- script.js
+++ script.js
@@ -1,157 +1,98 @@
-var apiKey = '0a6b68796bfcfe694987a9ddf3eddd1b735dcd7f';
-var urlBase = 'http://api.census.gov/data/2011/acs5';
-var current = $('#whichValue').val();
-var params = {
-	"for":'county:*',
-	"in":'state:*',
-	"key":apiKey,
-	"get":$('#whichValue option:selected').data('get')
-}
-$('#whichValue').on('change',function(){
-	current = $('#whichValue').val();
-	$.get(urlBase,{
-	"key":apiKey,
-	"get":$('#whichValue option:selected').data('get'),
-	"for":'county:*',
-	"in":'state:*'
-	},'json').then(function(a){
-		var metric = $('#whichValue option:selected').val();
-		var transform = vizes.findWhere({name:metric}).get('transform');
-		if (transform) {
-			buildValues(data, transform(a));
-		} else {
-			buildValues(data, a);
-		}
-		showAll();
-	});
+var Viz = Backbone.Model.extend({
+	defaults: {
+		transform:false,
+		stringRep:function(a){
+				return  parseInt(a,10).toString(10);
+			}
+	}
 });
-var m = L.map('map');
-if(!location.hash){
-	m.setView([42.076,-71.505], 8);
-}
-m.addHash();
-var MyControl = L.Control.extend({
-    options: {
-        position: 'bottomright'
-    },
 
-    onAdd: function (map) {
-        // create the control container with a particular class name
-        var container = L.DomUtil.create('div', 'my-custom-control leaflet-control-layers');
-container.id="legend";
-
-        return container;
-    }
+var Vizes = Backbone.Collection.extend({
+	model:Viz,
+	data:{}
 });
-m.addControl(new MyControl());
-var legend= new Legend({el:$('#legend')});
-var url = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpeg';
 
-var attributionText = 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+var vizes = new Vizes(items);
 
-var mapquestSubdomains = '1234';
-
-var optionsObject = {
-	attribution : attributionText,
-	subdomains : mapquestSubdomains
-};
-
-var mq=L.tileLayer(url, optionsObject).addTo(m);
-var watercolor = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg',{attribution:'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'});
-var data;
-var rt = makeRT({
-	initialize:function(){importScripts('js/rtree.js','js/topojson.js');this.rt=rTree()},
-	set:function(data,cb){this.rt.geoJSON(topojson.feature(data,data.objects.counties),function(err,data){if(!err){cb(true)}})},
-	bbox:function(data){return this.rt.bbox(data)}
+var Selector = Backbone.View.extend({
+	el:$('#whichValue'),
+	collection:vizes,
+	template:Mustache.compile('{{#items}}<option value="{{name}}" data-get="{{tables}}">{{name}}</option>{{/items}}'),
+	render:function(){
+		this.$el.html(this.template({items:this.collection.toJSON()}));
+	}
 });
-var obj={};
-var scale = d3.scale.quantile();
-scale.range(d3.range(11));
-var layerControl = L.control.layers({"Stamen Watercolor":watercolor,"Map Quest Open":mq}).addTo(m);
-var counties = L.geoJson({features:[]},{
-	onEachFeature:function (f,l){
-	var out = [];
-	if (f.properties){
-		for(var key in f.properties){
-			out.push(key+": "+f.properties[key]);
-		}
-	}	
-	var id=f.id.toString(10);
-	if(id.length === 4){
-		id = '0'+id;
+var selector = new Selector();
+selector.render();
+
+var Legend = Backbone.View.extend({
+	template:Mustache.compile('<strong>Legend</strong><ul class="legend">{{#items}}<li><span style="background: {{color}};"></span>{{value}}</li>{{/items}}</ul>'),
+	render:function(){
+		var vals = this.scale.quantiles().map(function(a,i){return {value:vizes.findWhere({name:$('#whichValue').val()}).get("stringRep")(a),color:colorbrewer.RdYlBu[11][i]}});
+		this.$el.html(this.template({items:vals}));
+	},collection:vizes,
+	initialize:function(){
+		this.collection.on('renderLegend',function(){this.render()},this);
+		this.scale = d3.scale.quantile();
+this.scale.range(d3.range(11));
 	}
-	//out.push("id: "+id);
-		if(obj[id]){
-			out.push(current+': '+obj[id].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-		
-		}
-		l.bindPopup(out.join("<br />"));
-	
-	},style:function(f){
-		var id=f.id.toString(10);
-	if(id.length === 4){
-		id = '0'+id;
-	}
-	if(obj[id]){
-			return {fillColor:colorbrewer.RdYlBu[11][scale(obj[id])],weight:0,fillOpacity:0.8};
-	}else{
-		return {stroke:false,fill:false};
-	}
-		
-	}
-}).addTo(m);
-function buildValues (data, rows){
-	
-var vals = [];
-	if(rows[0].length===4){
-		_.each(rows,function(r){
+});
+
+var Polys = Backbone.View.extend({
+	el:$('#whichValue'),
+	apiKey : '0a6b68796bfcfe694987a9ddf3eddd1b735dcd7f',
+	urlBase : 'http://api.census.gov/data/2011/acs5',
+	current : function(){return this.$el.val()},
+	collection:vizes,
+	obj:{},
+	params : function(){
+		return {
+		"for":'county:*',
+		"in":'state:*',
+		"key":this.apiKey,
+		"get":this.$('option:selected').data('get')
+	}},
+	events:{
+		'change':'valueChange'
+	},
+	buildValues:function(rows){
+		var self=this;
+		var vals = [];
+		if(rows[0].length===4){
+			_.each(rows,function(r){
+				var val = parseFloat(r[0],10);
+				self.obj[r[2]+r[3]]=val;
+				if(val){
+					vals.push(val);
+				}
+			});
+		}else if(rows[0].length===3){
+			_.each(rows,function(r){
 			var val = parseFloat(r[0],10);
-			obj[r[2]+r[3]]=val;
-			if(val){
-			vals.push(val);
+				self.obj[r[1]+r[2]]=val;
+				if(val){
+					vals.push(val);
+				}
+			});
+		}
+		self.options.legend.scale.domain(vals);
+		self.collection.trigger('renderLegend');
+	},
+	valueChange:function (){
+		var self = this;
+		$.get(this.urlBase,this.params(),'json').then(function(a){
+			var metric = $('#whichValue option:selected').val();
+			var transform = vizes.findWhere({name:metric}).get('transform');
+			if (transform) {
+				self.buildValues( transform(a));
+			} else {
+				self.buildValues(a);
 			}
-		});
-	}else if(rows[0].length===3){
-		_.each(rows,function(r){
-		var val = parseFloat(r[0],10);
-			obj[r[1]+r[2]]=val;
-			if(val){
-			vals.push(val);
-			}
+			updateMap();
 		});
 	}
-	scale.domain(vals);
-	legend.render();
-}
-	layerControl.addOverlay(counties,"Counties");
-//m.addHash({lc:layerControl});
-$.when($.ajax('json/us-10m.json'),$.ajax({url:urlBase,data:params,dataType:'jsonp',jsonp:'jsonp',cache:true})).then(function(a,b){
-	data = a[0];
-	var rows = b[0];
-	buildValues (data, rows);
-	
-	rt.set(data).then(function(){
-	
-			showAll();
-	
-	});
 });
 
-m.on("contextmenu moveend",showAll);
-function showAll(){
-	
-	var bounds = m.getBounds();
-	
-		rt.bbox(
-			[
-				[bounds.getSouthWest().lng,bounds.getSouthWest().lat],
-				[bounds.getNorthEast().lng,bounds.getNorthEast().lat]
-			]
-		).then(function(rData){
-	counties.clearLayers();
-		counties.addData(rData);
-		});
-}
 function makeRT(obj){
 	var newObj={};
 	if(typeof Worker === "function"){
@@ -172,5 +113,108 @@ function makeRT(obj){
 	}
 }
 
+//map stuff
+var m = L.map('map');
+if(!location.hash){
+	m.setView([42.076,-71.505], 8);
+}
+m.addHash();
+var MyControl = L.Control.extend({
+	options: {
+		position: 'bottomright'
+	},
 
+	onAdd: function (map) {
+		// create the control container with a particular class name
+		var container = L.DomUtil.create('div', 'my-custom-control leaflet-control-layers');
+		container.id="legend";
+		return container;
+	}
+});
+m.addControl(new MyControl());
+var url = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpeg';
+var attributionText = 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+var mapquestSubdomains = '1234';
+var optionsObject = {
+	attribution : attributionText,
+	subdomains : mapquestSubdomains
+};
+var mq=L.tileLayer(url, optionsObject).addTo(m);
+var watercolor = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg',{attribution:'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'});
+var layerControl = L.control.layers({"Stamen Watercolor":watercolor,"Map Quest Open":mq}).addTo(m);
 
+//start views
+var legend= new Legend({el:$('#legend')});
+var polys= new Polys({legend:legend});
+
+//set up webWorker
+var rt = makeRT({
+	initialize:function(){
+		importScripts('js/rtree.js','js/topojson.js');
+		this.rt=rTree();
+	},
+	set:function(data,cb){
+		this.rt.geoJSON(topojson.feature(data,data.objects.counties),function(err,data){
+			if(!err){
+				cb(true);
+			}
+		})
+	},
+	bbox:function(data){return this.rt.bbox(data)}
+});
+
+//create the counties layer
+var counties = L.geoJson({features:[]},{
+	onEachFeature:function (f,l){
+		var out = [];
+		if (f.properties){
+			for(var key in f.properties){
+				out.push(key+": "+f.properties[key]);
+			}
+		}	
+		var id=f.id.toString(10);
+		if(id.length === 4){
+			id = '0'+id;
+		}
+		if(polys.obj[id]){
+			out.push(polys.current()+': '+polys.obj[id].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+		}
+		l.bindPopup(out.join("<br />"));
+	},style:function(f){
+		var id=f.id.toString(10);
+		if(id.length === 4){
+			id = '0'+id;
+		}
+		if(polys.obj[id]){
+			return {fillColor:colorbrewer.RdYlBu[11][legend.scale(polys.obj[id])],weight:0,fillOpacity:0.8};
+		}else{
+			return {stroke:false,fill:false};
+		}
+	}
+}).addTo(m);
+//add it to the map
+layerControl.addOverlay(counties,"Counties");
+
+//download data to start us off
+$.when($.ajax('json/us-10m.json'),$.ajax({url:polys.urlBase,data:polys.params(),dataType:'jsonp',jsonp:'jsonp',cache:true})).then(function(a,b){
+	vizes.data = a[0];
+	var rows = b[0];
+	polys.buildValues (rows);
+	rt.set(vizes.data).then(function(){
+		updateMap();
+	});
+});
+
+m.on("contextmenu moveend",updateMap);
+function updateMap(){
+	var bounds = m.getBounds();
+	rt.bbox(
+		[
+			[bounds.getSouthWest().lng,bounds.getSouthWest().lat],
+			[bounds.getNorthEast().lng,bounds.getNorthEast().lat]
+		]
+	).then(function(rData){
+		counties.clearLayers();
+		counties.addData(rData);
+	});
+}
